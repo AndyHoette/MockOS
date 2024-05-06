e67aed3554146b1060106be428f4509819fae4b7
Peter Adams-Agresti <peteraa146@gmail.com>
2024-05-06 00:25:09 -0500

    ready

--- src/Studio17.cpp
+++ src/Studio17.cpp
@@ -5,9 +5,6 @@
 using namespace std;
 
 int main(){
-//    need this:
-//    Create a SimpleFileSystem object in main, create a few files and add them to the file
-//    system via the addFile method. Create a few files via the createFile method as well. Try
-//    opening, writing, reading, and deleting files to ensure things are working properly
+
 
 }
